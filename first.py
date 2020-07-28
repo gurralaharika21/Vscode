9f991ee83411bafa9eba62af2a5dc98f8d1db4f0
Gurrala Harika <gurralaharika21@msitprogram.com>
2020-07-28 17:03:20 +0530

    Jul 28  17:03:20

--- first.py
+++ first.py
@@ -1,2 +1,11 @@
-print("harika")
-print("vscode")
+# x = int(input())
+# values = [int(i) for i in input().strip().split()]
+# str1 = ""
+# for i in values:
+#     str1 += str(i)+" "
+    
+# s = str1[::-1]  
+# y = s.strip() 
+# print(y) 
+x = 2+5
+print(x)
